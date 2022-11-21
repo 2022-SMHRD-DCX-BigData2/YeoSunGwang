9a8a5864d99ed6e672d4fb84f45805cd8654a9aa
user <user@DESKTOP-U59IHVT>
2022-11-21 10:47:07 +0900

    삼행시

--- YeoSunGwang/src/main/java/test.java
+++ YeoSunGwang/src/main/java/test.java
@@ -4,6 +4,9 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println("팀장 커밋!!");
+		System.out.println("정");
+		System.out.println("상");
+		System.out.println("민");
 
 	}
 
