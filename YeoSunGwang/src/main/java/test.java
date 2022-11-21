eb406350f2b2fc5e864b7cd6a95e40a14b241de0
user <user@DESKTOP-U59IHVT>
2022-11-21 11:13:47 +0900

    이준혁 삼행시

--- YeoSunGwang/src/main/java/test.java
+++ YeoSunGwang/src/main/java/test.java
@@ -4,9 +4,9 @@ public class test {
 	public static void main(String[] args) {
 		
 		System.out.println("팀장 커밋!!");
-		System.out.println("정상민 섹시가이");
-		System.out.println("상민이 섹시가이");
-		System.out.println("민");
+		System.out.println("이");
+		System.out.println("준");
+		System.out.println("혁");
 
 	}
 
