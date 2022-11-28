a19263e62dd0cc847215a838c6887fc0555aa69c
JHJH93 <116530213+JHJH93@users.noreply.github.com>
2022-11-28 12:32:02 +0900

    123123

--- YeoSunGwang/src/main/java/com/smhrd/controller/JoinCon.java
+++ YeoSunGwang/src/main/java/com/smhrd/controller/JoinCon.java
@@ -44,15 +44,15 @@ public class JoinCon extends HttpServlet {
 			System.out.println("JoinCon : 회원가입 성공!");
 			// 회원가입 성공하면 joinSuccess.jsp로 이동
 //			response.sendRedirect("joinSuccess.jsp");
-			RequestDispatcher rd = request.getRequestDispatcher("joinSuccess.jsp");
-			request.setAttribute("joinid", id);
+			RequestDispatcher rd = request.getRequestDispatcher("MAIN.html");
+			request.setAttribute("join", id);
 			rd.forward(request, response);
 			
 			
 		} else {
 			System.out.println("JoinCon : 회원가입 실패!");
 			// 회원가입 실패하면 다시 회원가입 하게끔 main.jsp로 이동
-			response.sendRedirect("main.jsp");
+			response.sendRedirect("MAIN.html");
 		}
 		
 		
